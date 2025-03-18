ac12c306cf9c87a7131940e841ef530bd65c7778
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-03-19 05:08:35 +0900

    te4st

---
+++ api/index.test.ts
@@ -0,0 +1,14 @@
+import { app } from './index';
+describe('GET /api', () => {
+  it('should return 200', async () => {
+    const response = await app.request('/api/hello');
+    expect(response.status).toBe(200);
+    expect(await response.json()).toEqual({ message: 'Hello Hono!' });
+  })
+  it('should return 401', async () => {
+    const response = await app.request('/api/cron');
+    expect(response.status).toBe(401);
+    expect(await response.json()).toEqual({ message: 'Unauthorized' });
+    //expect(response.body).toEqual({ message: 'Unauthorized' });
+  })
+});
